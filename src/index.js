bc177ad08614df7f7ec86b3a542b423c6dd26ff0
Alex Aralis <alex.aralis@gmail.com>
2017-06-26 13:25:53 -0700

    set up basic test API route test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log(`slkdf ${'sdf'}`);
+import express from 'express';
+const app = express();
+
+// Apply middlewares
+import headersMiddleware from './middleware/headers';
+
+app.use(headersMiddleware);
+
+// Attach API routes
+import testRouter from './routes/test';
+
+app.use('/test', testRouter);
+
+const server = app.listen(process.env.PORT || 8888, () => {
+  console.log(`Listening on ${server.address().port}...`);
+});
