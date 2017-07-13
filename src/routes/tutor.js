065da265194b518a6f841eec6d83a90e2fb4e196
Alex Aralis <alex.aralis@gmail.com>
2017-07-12 17:00:49 -0700

    properly build the query object from the request

--- src/routes/tutor.js
+++ src/routes/tutor.js
@@ -1,18 +1,22 @@
 import { Router } from 'express';
-import { mapOn } from '../util';
 import User from '../models/User';
 
 const router = Router();
 
 router.get('/', (req, res) => {
-  User.find([
-    'zipCode',
-    'username',
-    'subjects',
-  ].reduce(
-    mapOn(req.body && req.body.query || {}),
-    { isTutor: true }
-  ))
+  const query = { isTutor: true };
+  const reqQuery = req.body.query;
+
+  if ('maxWage' in reqQuery || 'minWage' in reqQuery) query.wage = {};
+  if ('maxWage' in reqQuery) query.wage.$lt = reqQuery.maxWage;
+  if ('minWage' in reqQuery) query.wage.$gt = reqQuery.minWage;
+
+  if ('subjects' in reqQuery) query.subjects = { $in: reqQuery.subjects };
+
+  if ('zipCode' in reqQuery) query.zipCode = reqQuery.zipCode;
+  if ('username' in reqQuery) query.username = reqQuery.username;
+
+  User.find(query)
     .then(tutors => res.json({ tutors }))
     .catch(err => res.status(400).json({ err }));
 });
