3b5d22eb4aef4407417161b41e1f481deb822276
Alex Aralis <alex.aralis@gmail.com>
2017-07-26 10:46:00 -0700

    make sure learner has a card on appointment creation.

--- src/routes/appointment.js
+++ src/routes/appointment.js
@@ -8,6 +8,12 @@ const router = Router();
 router.post('/', (req, res) => {
   const learner = req.user;
 
+  if (!learner.card) {
+    res.status(400)
+      .json({ message: 'Learner must have a registered card to create an appointment.' });
+    return;
+  }
+
   User.find({ _id: req.body.tutor._id, isTutor: true })
     .then(tutor => new Appointment({
       tutor: tutor._id,
